6e7be58c3de5a4a9c187b77597aad01a45a6c157
Dirk Schaap <dwgs76@gmail.com>
2018-05-05 18:02:31 +0200

    Initial Commit

---
+++ programStart/ProgramStart.java
@@ -0,0 +1,13 @@
+package programStart;
+
+public class ProgramStart {
+
+	public static void main(String[] args) {
+		// this class will be used to experiment with Felix' one liners. 
+		// goal: try out everything I am not intimately familiar with.
+		// goal: build a wrong way-, and a right way version of eacht topic.
+		// goal: push both wrong way- and right way versions to GitHub as examples 
+
+	}
+
+}
